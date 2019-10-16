2616d0f849da74b2283d1f8b3e5779d487f99c58
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-15 20:56:14 -0500

    lesson 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Photo/Image Section';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.google.com/search?q=cardinals+images&client=firefox-b-1-d&tbm=isch&source=iu&ictx=1&fir=etS8UWCHjf8q_M%253A%252CiYP_G0jPyA73WM%252C_&vet=1&usg=AI4_-kTrx_cOrhGvwkMdOl8pFJbk8OlvTA&sa=X&ved=2ahUKEwjPuJTR0J_lAhVReKwKHQxOBskQ9QEwA3oECAQQCg#imgrc=etS8UWCHjf8q_M:';
+  image3 = 'https://www.google.com/search?q=cardinals+images&client=firefox-b-1-d&tbm=isch&source=iu&ictx=1&fir=etS8UWCHjf8q_M%253A%252CiYP_G0jPyA73WM%252C_&vet=1&usg=AI4_-kTrx_cOrhGvwkMdOl8pFJbk8OlvTA&sa=X&ved=2ahUKEwjPuJTR0J_lAhVReKwKHQxOBskQ9QEwA3oECAQQCg#imgrc=1vgthiUsGELHEM:&vet=1';
 
   constructor() { }
 
---
+++ lesson1/exercises/src/app/page-title/page-title.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'page-title',
+  templateUrl: './page-title.component.html',
+  styleUrls: ['./page-title.component.css']
+})
+export class PageTitleComponent implements OnInit {
+  title = 'Rebeccas Page'
+
+  constructor() {
+
+   }
+
+  ngOnInit() {
+  }
+
+}
